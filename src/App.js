811037560d07734fdcc73fda7abc45dc8b5f1946
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-17 11:37:57 +0300

    add main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </>
   );
 }
